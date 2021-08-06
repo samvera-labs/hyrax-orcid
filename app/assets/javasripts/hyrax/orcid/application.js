4df9745d330a4e7b77053c4d127372e20700cd5a
Paul Danelli <prdanelli@gmail.com>
2021-08-06 17:36:45 +0100

    Move js to partials

--- app/assets/javasripts/hyrax/orcid/application.js
+++ app/assets/javasripts/hyrax/orcid/application.js
@@ -1,12 +1,2 @@
 //= require_tree .
 
-  $(document).ready(function(){
-    $("body").on("click", ".js-orcid-sync-work-toggle", function(){
-      let attr = $(this).prop("checked") ? "on" : "off"
-
-      $.ajax({
-        dataType: "json",
-        url: $(this).data(`toggle-${attr}`),
-      });
-    });
-  })
---
+++ app/assets/javasripts/hyrax/orcid/dashboard.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("body").on("click", ".js-orcid-sync-work-toggle", function(){
+    let attr = $(this).prop("checked") ? "on" : "off"
+
+    $.ajax({
+      dataType: "json",
+      url: $(this).data(`toggle-${attr}`),
+    });
+  });
+})
+
