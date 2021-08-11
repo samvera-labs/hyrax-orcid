94299d7300781fe106b3a616f0f8d2103a07f2df
Paul Danelli <prdanelli@gmail.com>
2021-08-11 16:01:27 +0100

    Add presenter specs

---
+++ app/presenters/hyrax/orcid/profile/employment_presenter.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Hyrax
+  module Orcid
+    module Profile
+      class EmploymentPresenter < BasePresenter
+        def key
+          "employment"
+        end
+
+        def collection
+          @collection.map do |entry|
+            {
+              title: entry["role-title"],
+              items: [
+                "#{date_from_hash(entry['start-date'])} - #{date_from_hash(entry['end-date'])}",
+                entry["department-name"],
+                entry.dig("organization", "name"),
+                address_from_hash(entry.dig("organization", "address"))
+              ].reject(&:blank?)
+            }
+          end
+        end
+      end
+    end
+  end
+end
