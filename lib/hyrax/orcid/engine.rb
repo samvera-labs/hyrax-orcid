2b79fea2b9b3b2214695cb8131eafffd7ccf8cf2
Paul Danelli <prdanelli@gmail.com>
2021-08-25 10:22:33 +0100

    Update bolognese now PR#121 merged

--- lib/hyrax/orcid/engine.rb
+++ lib/hyrax/orcid/engine.rb
@@ -29,9 +29,39 @@ module Hyrax
       end
 
       def self.dynamically_include_mixins
+        # NOTE: This is a temp fix because of development loading issues
         User.include Hyrax::Orcid::UserBehavior
+        # ::User.class_eval do
+        #   has_one :orcid_identity, dependent: :destroy
 
+        #   def orcid_identity_from_authorization(params)
+        #     transformed = params.symbolize_keys
+        #     transformed[:orcid_id] = transformed.delete(:orcid)
 
+        #     create_orcid_identity(transformed)
+        #   end
+
+        #   def orcid_identity?
+        #     orcid_identity.present?
+        #   end
+
+        #   def orcid_referenced_works
+        #     @_orcid_referenced_works ||= begin
+        #       return [] if orcid_identity.blank?
+
+        #       # NOTE: I'm trying to avoid returning ID's and performing a Fedora query if I can help it,
+        #       # but if we need to instantiate the Model objects, this can be done by returning just the ID
+        #       # options = { fl: [:id], rows: 1_000_000 }
+
+        #       # For some reason, `'` causes the query to return no results, so we need to use `\"`
+        #       id = orcid_identity.orcid_id
+        #       query_string = "(contributor_tesim:\"*#{id}*\" OR creator_tesim:\"*#{id}*\") AND visibility_ssi:open"
+        #       result = ActiveFedora::SolrService.get(query_string, row: 1_000_000)
+
+        #       result['response']['docs'].map { |doc| ActiveFedora::SolrHit.new(doc) }
+        #     end
+        #   end
+        # end
 
         # Add any required helpers, for routes, api metadata etc
         Hyrax::HyraxHelperBehavior.include Hyrax::Orcid::HelperBehavior
@@ -46,9 +76,6 @@ module Hyrax
         # Insert our custom reader and writer to process works ready before publishing
         Bolognese::Metadata.prepend Bolognese::Writers::Orcid::XmlWriter
         Bolognese::Metadata.prepend Bolognese::Readers::Orcid::HyraxWorkReader
-
-        # TODO: Remove this when PR#121 is merged
-        Bolognese::Metadata.prepend Bolognese::UtilsBehaviors
 
         # Because the Hyrax::ModelActor does not call next_actor to continue the chain,
         # for destroy requests, we require a new actor
