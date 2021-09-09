f42c698cab8beafa9073996543a1d50ed044bbf4
Paul Danelli <prdanelli@gmail.com>
2021-09-09 13:35:37 +0100

    Remove old test code

--- spec/models/orcid_identity_spec.rb
+++ spec/models/orcid_identity_spec.rb
@@ -14,23 +14,4 @@ RSpec.describe OrcidIdentity, type: :model do
   it { is_expected.to define_enum_for(:work_sync_preference).with_values(work_sync_preference) }
   it { is_expected.to belong_to(:user).class_name("User") }
   it { is_expected.to have_many(:orcid_works) }
-
-  # TODO: Is this necessary?
-  #
-  # describe "#published_works" do
-  #   let(:service_class) { Hyrax::Orcid::Work::ReaderService }
-  #   let(:service) { instance_double(service_class, read: []) }
-  #   let(:user) { create(:user) }
-  #   let!(:orcid_identity) { create(:orcid_identity, user: user) }
-
-  #   before do
-  #     allow(orcid_identity).to receive(:published_works).and_return(service)
-  #   end
-
-  #   it "calls the reader service" do
-  #     orcid_identity.published_works
-
-  #     expect(service).to have_received(:read)
-  #   end
-  # end
 end
