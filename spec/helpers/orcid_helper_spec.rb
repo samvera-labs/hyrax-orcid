471a2f583421492f5b1b17f1b9d477e38623715d
Paul Danelli <prdanelli@gmail.com>
2021-09-23 15:18:49 +0100

    Use controller helper to remove monkey patch from spec

--- spec/helpers/orcid_helper_spec.rb
+++ spec/helpers/orcid_helper_spec.rb
@@ -1,8 +1,13 @@
+# frozen_string_literal: true
 # frozen_string_literals: true
 
 require "rails_helper"
 
 RSpec.describe Hyrax::Orcid::OrcidHelper do
+  include Devise::Test::ControllerHelpers
+
+  let(:helper) { _view }
+
   let(:domain) { "https://orcid.org/" }
   let(:sandbox_domain) { "https://sandbox.orcid.org/" }
   let(:orcid) { "0000-1234-1234-1234" }
@@ -10,32 +15,26 @@ RSpec.describe Hyrax::Orcid::OrcidHelper do
   let(:orcid_with_no_delimiter) { "0000123412341234" }
   let(:x_orcid) { "0000-1234-1234-123X" }
   let(:invalid_orcid) { "this-is-not-an-orcid" }
-  let(:klass) do
-    class TestClass
-      include Hyrax::Orcid::OrcidHelper
-    end
-  end
-  subject(:subject) { klass.new }
 
   describe "validate_orcid" do
-    it { expect(subject.validate_orcid(orcid)).to eq(orcid) }
-    it { expect(subject.validate_orcid(sandbox_domain + orcid)).to eq(orcid) }
-    it { expect(subject.validate_orcid(domain + orcid)).to eq(orcid) }
+    it { expect(helper.validate_orcid(orcid)).to eq(orcid) }
+    it { expect(helper.validate_orcid(sandbox_domain + orcid)).to eq(orcid) }
+    it { expect(helper.validate_orcid(domain + orcid)).to eq(orcid) }
 
-    it { expect(subject.validate_orcid(orcid_with_spaces)).to eq(orcid) }
-    it { expect(subject.validate_orcid(sandbox_domain + orcid_with_spaces)).to eq(orcid) }
-    it { expect(subject.validate_orcid(domain + orcid_with_spaces)).to eq(orcid) }
+    it { expect(helper.validate_orcid(orcid_with_spaces)).to eq(orcid) }
+    it { expect(helper.validate_orcid(sandbox_domain + orcid_with_spaces)).to eq(orcid) }
+    it { expect(helper.validate_orcid(domain + orcid_with_spaces)).to eq(orcid) }
 
-    it { expect(subject.validate_orcid(orcid_with_no_delimiter)).to eq(orcid) }
-    it { expect(subject.validate_orcid(sandbox_domain + orcid_with_no_delimiter)).to eq(orcid) }
-    it { expect(subject.validate_orcid(domain + orcid_with_no_delimiter)).to eq(orcid) }
+    it { expect(helper.validate_orcid(orcid_with_no_delimiter)).to eq(orcid) }
+    it { expect(helper.validate_orcid(sandbox_domain + orcid_with_no_delimiter)).to eq(orcid) }
+    it { expect(helper.validate_orcid(domain + orcid_with_no_delimiter)).to eq(orcid) }
 
-    it { expect(subject.validate_orcid(x_orcid)).to eq(x_orcid) }
-    it { expect(subject.validate_orcid(sandbox_domain + x_orcid)).to eq(x_orcid) }
-    it { expect(subject.validate_orcid(domain + x_orcid)).to eq(x_orcid) }
+    it { expect(helper.validate_orcid(x_orcid)).to eq(x_orcid) }
+    it { expect(helper.validate_orcid(sandbox_domain + x_orcid)).to eq(x_orcid) }
+    it { expect(helper.validate_orcid(domain + x_orcid)).to eq(x_orcid) }
 
-    it { expect(subject.validate_orcid(invalid_orcid)).to be_nil }
-    it { expect(subject.validate_orcid(sandbox_domain + invalid_orcid)).to be_nil }
-    it { expect(subject.validate_orcid(domain + invalid_orcid)).to be_nil }
+    it { expect(helper.validate_orcid(invalid_orcid)).to be_nil }
+    it { expect(helper.validate_orcid(sandbox_domain + invalid_orcid)).to be_nil }
+    it { expect(helper.validate_orcid(domain + invalid_orcid)).to be_nil }
   end
 end
