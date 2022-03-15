eab774664c8c86e8044d90323b2e3f412a055975
Paul Danelli <prdanelli@gmail.com>
2022-03-15 16:43:02 +0000

    Add the default configuration initalizer, to prevent the app not starting up in development

---
+++ config/initializers/hyrax/orcid/configure.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Add your configuration here
+Hyrax::Orcid.configure do |config|
+#   # :sandbox or :production
+#   config.environment = :sandbox
+#
+#   config.auth = {
+#     client_id: "YOUR-APP-ID",
+#     client_secret: "your-secret-token",
+#     # The authorisation return URL you entered when creating the Orcid Application. Should be your repository URL and `/dashboard/orcid_identity/new`
+#     redirect_url: "http://your-repo.com/dashboard/orcid_identity/new"
+#   }
+end
